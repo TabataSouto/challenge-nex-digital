aea97ab4b9a9d07ce2b416b61e95283c44be75a9
Tabata Souto <tabatac.souto@gmail.com>
2024-07-24 13:04:13 -0300

    feat: criação migrations para users e transactions

---
+++ src/database/migrations/20240724154546_transactions.ts
@@ -0,0 +1,16 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("transactions", (table) => {
+    table.increments("id").primary();
+    table.string("cpf").notNullable();
+    table.string("description").notNullable();
+    table.date("transaction_date").notNullable();
+    table.decimal("points_value", 10, 3).notNullable();
+    table.decimal("amount_value", 10, 2).notNullable();
+    table.string("status").notNullable();
+    table.timestamps(false, false);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {}
