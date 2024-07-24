88f3878e279729b512d11604aeef2007d04200a7
Tabata Souto <tabatac.souto@gmail.com>
2024-07-24 13:03:50 -0300

    feat: conexão banco de dados

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -3,27 +3,29 @@ import "dotenv/config.js";
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+//  configuração para conexão com banco de dados
 export default {
   development: {
-    client: "mysql",
+    client: "mysql2",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: "127.0.0.1",
+      port: 3306,
+      user: "root",
+      password: "root",
+      database: "digital_nex_challenge",
     },
     migrations: {
       directory: "./database/migrations",
     },
   },
-
   production: {
-    client: "mysql",
+    client: "mysql2",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_NAME,
     },
     migrations: {
       directory: "./database/migrations",
