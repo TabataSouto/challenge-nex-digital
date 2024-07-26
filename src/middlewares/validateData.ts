24f18033a15cd22e62fb01a1713d2af6304c1f05
Tabata Souto <tabatac.souto@gmail.com>
2024-07-26 13:33:54 -0300

    fix: correção validação de informações para cadastro.

--- src/middlewares/validateData.ts
+++ src/middlewares/validateData.ts
@@ -6,12 +6,18 @@ const validateData = (
   next: NextFunction
 ) => {
   const { name, email, password, cpf } = request.body;
+  const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*\-+?]{8,}$/;
 
   !name || !email || !password || !cpf
     ? next({
         status: 406,
+        message: "Os campos Nome, Email, Senha e CPF devem ser preenchidos!",
+      })
+    : !regex.test(password)
+    ? next({
+        status: 406,
         message:
-          "Os campos Nome, Email, Senha e CPF devem ser preenchidos!",
+          "A senha precisa ter 1 caracter maíusculo, 1 caracter minúsculo, 1 dígito e no mínimo de 8 caracteres",
       })
     : next();
 };
